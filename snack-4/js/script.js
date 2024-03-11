992bedbfdb3161702275a6372dd3c856b1d87a3b
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-11 18:06:06 +0100

    feat: sum of the elements of the array

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -2,10 +2,17 @@
 
 
 // creare un array con 10 numeri
-const numberList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(numberList)
 
-// sommare tutti e 10 i numeri
+// sommare tutti e 10 i numeri (1 + 2 + 3 + 4 + 5 + 6.... sono elementi della lista)
+let sum = 0;
+for (let i = 0; i < numberList.length; i++){
+    
+
+    sum = sum + (numberList[i]);
+    console.log(sum)
+}
 
 // prendere il risultato
 // dividerlo per 10
