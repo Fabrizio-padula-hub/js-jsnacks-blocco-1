4284543dede0dfbcfc573a3f31b51f2df9345c37
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-11 17:54:13 +0100

    feat: add list array

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,1 +1,11 @@
 // Calcola la somma e la media dei primi 10 numeri.
+
+
+// creare un array con 10 numeri
+const numberList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+console.log(numberList)
+
+// sommare tutti e 10 i numeri
+
+// prendere il risultato
+// dividerlo per 10
