e58c331d6a42d32db8752b96547c7d17716566c0
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-11 18:17:50 +0100

    feat: added the sum of the numbers and the average

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -3,16 +3,18 @@
 
 // creare un array con 10 numeri
 const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(numberList)
 
 // sommare tutti e 10 i numeri (1 + 2 + 3 + 4 + 5 + 6.... sono elementi della lista)
 let sum = 0;
 for (let i = 0; i < numberList.length; i++){
     
-
-    sum = sum + (numberList[i]);
-    console.log(sum)
+    sum = sum + numberList[i];
+    console.log('somma elementi' + sum)
 }
 
 // prendere il risultato
 // dividerlo per 10
+const average = sum / 10;
+console.log(`Media ${average}`)
+
+alert(`la somma dei numeri è ${sum}, la media è ${average}`);
