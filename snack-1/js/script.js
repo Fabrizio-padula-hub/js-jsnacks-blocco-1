50c60a6e029dbf774d77d004dde4c9f1271b4498
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-11 16:42:54 +0100

    feat: addition of prompt to ask for numbers and addition of if to analyze them

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,1 +1,24 @@
 // Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+
+// aprire l'array
+const numberList = [];
+
+
+// chiedere all'utente i 5 numeri
+for (let i = 0; i < 5; i++){
+    console.log(numberList)
+
+    const userNumber = parseInt(prompt('Scrivi i tuoi numeri'));
+    console.log('numeri utente', userNumber)
+
+    // inserire i numeri nell'array ma
+    // se i numeri inseriti sono uguali a quelli nella lista, non inserirli
+    if (numberList.includes(userNumber) === false){
+        numberList.push(userNumber);
+
+    }
+
+}
+console.log(numberList)
+
+    
