e522d55fcf90e4285e3f0a7f9e6dcbac13bfabb0
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-11 16:57:08 +0100

    feat: addition of prompts, addition of number list

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,2 +1,19 @@
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
+
+const listNumber = [];
+
+// chiedere all'utente 5 numeri
+for (let i = 0; i < 5; i++){
+
+    const userNumber = parseInt(prompt('Scrivi il tuo numero'));
+    console.log('numero utente', userNumber)
+
+    // aggiungere i numeri in un recipiente
+    listNumber.push(userNumber);
+    console.log(listNumber)
+
+}
+
+// sommare tutti i numeri dell'utente
+// stampare la somma di tutti i numeri
