cd024ee39e72c7c0fd768e4eca9550ca2cec6ecb
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-11 17:32:21 +0100

    feat: adding program for odd or even and inserting into array

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -8,11 +8,16 @@ const listNumber = [];
 
 // chiedere all'utente di scrivere 6 numeri diversi
 for (let i = 0; i < 6; i++){
-    console.log(i)
 
     const numberUser = parseInt(prompt('Scrivi i tuoi numeri'));
     console.log('numeri utente', numberUser)
-}
 
-// prendere i numeri dell'utente e vedere se sono pari o dispari
-// se i numeri sono dispari inserirli nell'array
+    // prendere i numeri dell'utente e vedere se sono pari o dispari
+    // se i numeri sono dispari inserirli nell'array
+    if(numberUser % 2 === 1){
+        listNumber.push(numberUser);
+    }
+
+
+}
+alert(`I numeri dispari sono: ${listNumber}`);
